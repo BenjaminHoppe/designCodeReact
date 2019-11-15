2ed5d5e532ffd74d9e11966ffa5ac8f0d6a97b03
Benjamin Hoppe <hoppeconnect@gmail.com>
2019-11-15 15:56:20 -0500

    made header change on scroll

--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,44 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
+import { Link } from "gatsby"
 import logo from '../images/logo-designcode.svg';
 import './header.css';
 
-const Header = ({ siteTitle }) => (
- <div className="header">
-   <div className="headerGroup">
-     <Link to="/"><img src={logo} width="30"/></Link>
-     <Link to="/courses">Courses</Link>
-     <Link to="/downloads">Downloads</Link>
-     <Link to="/workshops">Workshops</Link>
-     <button><Link to="/buy">Buy</Link></button>
-    
-   </div>
- </div>
-)
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state= {
+      hasScrolled: false
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll = (event) => {
+    const scrollTop = window.pageYOffset
+
+    if (scrollTop > 50) {
+      this.setState({hasScrolled: true})
+    } else {
+      this.setState({hasScrolled: false})
+    }
+  }
+
+  render() {
+    return(
+      <div className={this.state.hasScrolled ? 'header headerScrolled' : 'header'}>
+      <div className="headerGroup">
+        <Link to="/"><img src={logo} width="30"/></Link>
+        <Link to="/courses">Courses</Link>
+        <Link to="/downloads">Downloads</Link>
+        <Link to="/workshops">Workshops</Link>
+        <button><Link to="/buy">Buy</Link></button>
+      </div>
+    </div>
+    )
+  }
+}
 
 export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -44,10 +44,24 @@ const IndexPage = () => (
     </div>
     <div className="cards">
       <h2>11 courses, more coming</h2>
-      <Card 
-        title="React for Designers"
-        text="12 sections"
-        image={require('../images/wallpaper.jpg')} />
+      <div className="cardGroup">
+        <Card 
+          title="React for Design System"
+          text="10 sections"
+          image={require('../images/wallpaper.jpg')} />
+        <Card 
+          title="React for Designers"
+          text="12 sections"
+          image={require('../images/wallpaper2.jpg')} />
+        <Card 
+          title="Sound Design"
+          text="5 sections"
+          image={require('../images/wallpaper3.jpg')} />
+        <Card 
+          title="ARKit 2"
+          text="10 sections"
+          image={require('../images/wallpaper4.jpg')} />
+      </div>
     </div>
   </Layout>
 )
